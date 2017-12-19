2618ada48c75759bf63efee940b9514bb0111c01
hrvooje <hrvooje@gmail.com>
2017-12-19 14:13:09 +0100

    Maknio XML. Dodao samo kreiranje txt datoteke. Ako nema C:\PCT mape onda kreira samo lokalno

--- system_report.py
+++ system_report.py
@@ -10,27 +10,33 @@ Uz pomoć CPU-Z programa uzima podatke o OS-u i hardveru i konvertira
 datoteku u xml
 """
 
-import subprocess
-from bs4 import BeautifulSoup
 import os
+import socket
+import subprocess
+from shutil import copyfile
 
 
 # Saznaje ime lokacije ili ime računala radi imenovanja datoteke
-for root, dirs, files in os.walk("C:\PCT"):
+# Prvo postavi da se file zove kao lokalno računalo
+store_name = socket.gethostname()
+for root, dirs, files in os.walk("C:\\PCT"):
+    # Onda potraži datoteku C:\PCT\ime_prodavaonice.who ako ima pa tako imenuj
     for file in files:
-        if file.endswith(".who"):
-            #print(os.path.join(root, file))
+        if file.endswith(".who") or file.endswith(".WHO"):
             # Odsjeci nastavak filea u C:\PCT\store_name.who na samo store_name
             store_name = file[0:-4]
 
+# Varijable programa koji treba izvesti i datoteka u koju se sprema rezultat
 cpuz = "cpuz_x32.exe"
-param = "-html=" + store_name
+param = "-txt=" + store_name
+fname = store_name + ".txt"
 
-# Pozovi CPU-Z i kreiraj htm datoteku naziva u 'param' varijabli
+# Pozovi CPU-Z i kreiraj txt datoteku naziva u 'param' varijabli
 subprocess.call([cpuz, param])
 
-# Pretvori html u xml
-with open(store_name + ".htm") as f:
-    soup = BeautifulSoup(f, "html.parser")
-with open(store_name + ".xml", 'w') as g:
-    print(soup.prettify(), file=g)
+# Kopiraj datoteke u C:\PCT\ i u datoteku anketa.txt za slanje na server
+try:
+    copyfile(fname, 'C:\\PCT\\anketa.txt')
+    copyfile(fname, 'C:\\PCT\\' + fname)
+except Exception as e:
+    pass
